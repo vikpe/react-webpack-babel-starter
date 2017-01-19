17b7b3cf974f4b9d73aac84e8cd7609c4be3da88
Viktor Persson <viktor.persson@arcsin.se>
2017-01-19 17:22:10 +0100

    Defined App as a React Component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,14 @@ require("!style-loader!css-loader!sass-loader!./App.scss");
 
 const reactLogo = require('./react_logo.svg');
 
-const App = () => (
-    <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the bar</p>
-        <img src={reactLogo}/>
-    </div>
-);
+class App extends React.Component {
+    render() {
+        return <div className="app">
+            <h1>Hello World!</h1>
+            <p>Foo to the bar</p>
+            <img src={reactLogo}/>
+        </div>;
+    }
+}
 
 export default App;
