d8f36007caf04968c1a7d2c9f0e236ba9f88441e
Viktor Persson <viktor.persson@arcsin.se>
2017-01-19 15:35:08 +0100

    Updated webpack config; process .jsx files with Babel.

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test:    /\.js$/,
+                test:    /\.(js|jsx)$/,
                 use:     ["babel-loader"],
                 exclude: /node_modules/
             },
