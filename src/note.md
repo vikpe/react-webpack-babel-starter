c36dc8a135c4384b1c79db388e2a6c80e88c7a7c
jin wang <v-jiwag@microsoft.com>
2017-05-24 11:02:59 +0800

    ts integrate webpack

---
+++ src/note.md
@@ -0,0 +1,10 @@
+
+# webpack usage for 
+# https://webpack.js.org/guides/development/#adjusting-your-text-editor
+
+# how express serve static content 
+# https://github.com/webpack/webpack-dev-middleware/issues/189
+
+
+# use webpack dev middleware in Express 
+# http://madole.github.io/blog/2015/08/26/setting-up-webpack-dev-middleware-in-your-express-application/
--- webpackConfigs/webpack.config.js
+++ webpackConfigs/webpack.config.js
@@ -7,23 +7,25 @@ module.exports = {
         extensions: [".webpack.js", ".web.js", ".js", ".jsx", ".tsx", ".ts"],
     },
     entry:   [
+        /*
         "react-hot-loader/patch", // activate HMR for React
         "webpack-dev-server/client?http://localhost:8080",// bundle the client for webpack-dev-server and connect to the provided endpoint
         "webpack/hot/only-dev-server", // bundle the client for hot reloading, only- means to only hot reload for successful updates
+        */
         "./index.js" // the entry point of our app
     ],
     output:  {
         filename:   "bundle.js", // the output bundle
-        path:       resolve(__dirname, "public"),
+        path:       resolve(__dirname, "../public"),
         publicPath: "/" // necessary for HMR to know where to load the hot update chunks
     },
 
-    context: resolve(__dirname, "src"),
+    context: resolve(__dirname, "../src"),
     devtool: "inline-source-map",
 
     devServer: {
         hot:         true, // enable HMR on the server
-        contentBase: resolve(__dirname, "public"), // match the output path
+        contentBase: resolve(__dirname, "../public"), // match the output path
         publicPath:  "/" // match the output `publicPath`
     },
 
@@ -59,7 +61,7 @@ module.exports = {
 
     plugins:     [
         new StyleLintPlugin(),
-        new webpack.HotModuleReplacementPlugin(), // enable HMR globally
+        //new webpack.HotModuleReplacementPlugin(), // enable HMR globally
         new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
 
     ],
