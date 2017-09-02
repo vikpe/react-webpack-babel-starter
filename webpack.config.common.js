2d5b267706f801eca72fa737e3db72524071b021
Viktor Persson <viktor.persson@arcsin.se>
2017-09-02 13:04:41 +0200

    Added production build script (npm run start-prod). Builds app once (HMR disabled) and serves app @ http://localhost:3000

---
+++ webpack.config.common.js
@@ -0,0 +1,49 @@
+// shared config (dev and prod)
+const webpack         = require('webpack');
+const {resolve}       = require('path');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.js', '.jsx'],
+  },
+  context: resolve(__dirname, 'src'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?modules', 'postcss-loader',],
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules',
+          'postcss-loader',
+          'sass-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new StyleLintPlugin(),
+  ],
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+  },
+  performance: {
+    hints: false,
+  },
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,25 @@
+// production config
+const merge     = require('webpack-merge');
+const webpack   = require('webpack');
+const {resolve} = require('path');
+
+const commonConfig = require('./webpack.config.common');
+
+module.exports = merge(commonConfig, {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.min.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: false,
+      minimize: true,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+    }),
+  ],
+});
