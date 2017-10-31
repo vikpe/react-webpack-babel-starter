3e82f2a87af4027118bfc62ac3ef815e1d97d95a
Viktor Persson <viktor.persson@arcsin.se>
2017-10-31 23:29:09 +0100

    Refactored App; moved assets to separate folder, changed App to PureComponent.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
+import '!style-loader!css-loader!sass-loader!./../assets/scss/App.scss';
+import reactLogo from './../assets/img/react_logo.svg';
 
-require("!style-loader!css-loader!sass-loader!./App.scss");
-
-const reactLogo = require('./react_logo.svg');
-
-class App extends React.Component {
-    render() {
-        return <div className="app">
-            <h1>Hello World!</h1>
-            <p>Foo to the bar</p>
-            <img src={reactLogo}/>
-        </div>;
-    }
+class App extends React.PureComponent {
+  render() {
+    return (
+        <div className="app">
+          <h1>Hello World!</h1>
+          <p>Foo to the bar</p>
+          <img src={reactLogo}/>
+        </div>
+    );
+  }
 }
 
 export default App;
