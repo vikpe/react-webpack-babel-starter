cfa66111fd8acec4954e0313754b1e12906a3bce
Viktor Persson <viktor.persson@arcsin.se>
2018-08-22 13:51:43 +0200

    Removed postcss-loader.

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -18,8 +18,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          {loader: 'css-loader', options: {importLoaders: 1}},
-          'postcss-loader',
+          {loader: 'css-loader', options: {importLoaders: 1}}
         ],
       },
       {
@@ -27,7 +26,6 @@ module.exports = {
         loaders: [
           'style-loader',
           {loader: 'css-loader', options: {importLoaders: 1}},
-          'postcss-loader',
           'sass-loader',
         ],
       },
