7b2c129c319fb0dbd944d03f0c712ba8bdc7a56b
Viktor Persson <viktor.persson@arcsin.se>
2021-11-14 15:23:07 +0100

    Update dependencies.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { hot } from 'react-hot-loader';
 import 'assets/scss/App.scss';
 import AboutPage from 'components/AboutPage';
 import WelcomePage from 'components/WelcomePage';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
@@ -14,8 +14,10 @@ class App extends React.Component {
             <Link to="/">Home</Link> | <Link to="/about">About</Link>
           </nav>
           <hr />
-          <Route exact path="/" component={WelcomePage} />
-          <Route path="/about" component={AboutPage} />
+          <Routes>
+            <Route exact path="/" element={<WelcomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
         </div>
       </BrowserRouter>
     );
